d4d9b80d1afd6940c3156932d8adf6f3dafecc41
99davidscg <99davidscg@gmail.com>
2024-01-16 16:10:55 +0100

    Agregar .gitignore Y crear proyecto

---
+++ PaqueteriaApi/Entidades/UbicacionHistorial.cs
@@ -0,0 +1,10 @@
+﻿namespace PaqueteriaApi.Entidades
+{
+    public class UbicacionHistorial
+    {
+        public int id { get; set; }
+        public int id_vehiculo { get; set; }
+        public string ubicacion { get; set; }
+        public DateTime fecha { get; set; }
+    }
+}
---
+++ PaqueteriaApi/Entidades/Vehiculo.cs
@@ -0,0 +1,10 @@
+﻿namespace PaqueteriaApi.Entidades
+{
+    public class Vehiculo
+    {
+        public int id { get; set; }
+        public string matricula { get; set; }
+        public string abicacion_actual { get; set; }
+        public List<UbicacionHistorial> lst_historial_ubicaciones { get; set; }
+    }
+}
